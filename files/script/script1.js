40bb129067068060f59c4da455e21d0bb5c5a798
AWM006 <sunitamondal7924@gmail.com>
2025-02-19 20:47:03 +0530

    now optimized for screen resolution

--- files/script/script1.js
+++ files/script/script1.js
@@ -1,53 +1,62 @@
 
 
 //for mouse over and sound on
-let aa = document.querySelector(".mmm3");
 let pp = document.querySelector(".video");
+let yo = document.querySelector(".hello");
+let audio0 = document.querySelector(".audio0");
+let mmm1 = document.querySelector(".mmm1");
+let mmm2 = document.querySelector(".mmm2");
+let mmm3 = document.querySelector(".mmm3");
+let mmm4 = document.querySelector(".mmm4");
+let mmm5 = document.querySelector(".mmm5");
+let audio1 = document.querySelector(".audio1");
+let audio2 = document.querySelector(".audio2");
 
 pp.addEventListener("mouseover", function(){
     pp.muted = false;
+    mmm2.style.height = "100%";
 });
 pp.addEventListener("mouseout",function(){
     pp.muted = true ;
+    mmm2.style.height = "80%";
 });
-
-
-let yo = document.querySelector(".hello");
 
 yo.addEventListener("mouseover", function(){
     yo.muted = false;
+    mmm4.style.height = "100%";
 });
 yo.addEventListener("mouseout",function(){
     yo.muted = true ;
+    mmm4.style.height = "80%";
 }); 
 
 //for play audio
 
-let mmm5 = document.querySelector(".mmm5");
-let audio0 = document.querySelector(".audio0");
 mmm5.addEventListener("mouseover", function(){
-    audio0.play();
+    audio2.play();
+    mmm5.style.height = "100%";
 });
 mmm5.addEventListener("mouseout",function(){
-    audio0.pause();
+    audio2.pause();
+    mmm5.style.height = "80%";
 });
 
-let mmm1 = document.querySelector(".mmm1");
-let audio1 = document.querySelector(".audio1");
 mmm1.addEventListener("mouseover", function(){
     audio1.play();
+    mmm1.style.height = "100%";
 });
 mmm1.addEventListener("mouseout",function(){
     audio1.pause();
+    mmm1.style.height = "80%";
 });
 
-let mmm2 = document.querySelector(".mmm2");
-let audio2 = document.querySelector(".audio2");
-mmm2.addEventListener("mouseover", function(){
-    audio2.play();
+mmm3.addEventListener("mouseover", function(){
+    audio0.play();
+    mmm3.style.height = "100%";
 });
-mmm2.addEventListener("mouseout",function(){
-    audio2.pause();
+mmm3.addEventListener("mouseout",function(){
+    audio0.pause();
+    mmm3.style.height = "80%";
 });
 
 let switch1 = document.querySelector(".switch1");
